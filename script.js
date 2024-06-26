dabf7f841ab8dfe7b4fcb286c05137caceafa26b
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-26 00:36:11 -0700

    save

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-document.addEventListener('dblclick', () => {
-    if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
+//////////////////// SETTING VARS ////////////////////
+const PAGE_URL = location.toString();
+const WEB_URL = "https://sketcheddoughnut.github.io/SketchedDoughnut/";
+
+//////////////////// PAGE DETECTING ////////////////////
+// redirect to index.html
+if (PAGE_URL === WEB_URL) {
+    window.location.href = "https://sketcheddoughnut.github.io/SketchedDoughnut/index.html";
+}
+
+// detect page that they are on, run the loading function for that page
+if (PAGE_URL.includes('color.html')) {
+    colorOnLoad();
+}
+
+//////////////////// ALL LOADER FUNCTIONS ////////////////////
+// sets up things for the colors page upon call
+function colorOnLoad() {
+    document.addEventListener('dblclick', () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
         }
-    }
-});
+    });
+}
