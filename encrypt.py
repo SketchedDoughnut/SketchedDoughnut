5574394d2bf6d424032a75fb1993b80160a98b58
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-09-04 17:39:00 -0700

    pplease?

--- encrypt.py
+++ encrypt.py
@@ -18,6 +18,7 @@ def encrypt_data():
     output = input('please input the output path: ')
     main_key = input('please input the main key: ')
     secondary_key = input('please input the secondary key: ')
+    encrypt_or_decrypt = input('please choose to encrypt (e), decrypt (d), recompile (r): ').lower()
 
     # create tools
     main_tool = Fernet(main_key)
@@ -27,18 +28,56 @@ def encrypt_data():
     f = open(source, 'r')
     raw_data = f.read()
     f.close()
-    print(f'[green]raw data from file [purple]{source}: [red]{raw_data}')
+    print(f'[green]read raw data from file [purple]{source}: [red]{raw_data}')
 
-    # encode, then encrypt data
-    encoded_data = raw_data.encode()
-    main_encrypted_data = main_tool.encrypt(encoded_data)
-    secondary_encrypted_data = secondary_tool.encrypt(main_encrypted_data)
-    print(f'[green]double encrypted data: [red]{secondary_encrypted_data}')
+    # if encrypt, encrypt and dump data
+    if encrypt_or_decrypt == 'e':
+        # encode, then encrypt data
+        encoded_data = raw_data.encode()
+        main_encrypted_data = main_tool.encrypt(encoded_data)
+        secondary_encrypted_data = secondary_tool.encrypt(main_encrypted_data)
+        print(f'[green]double encrypted data: [red]{secondary_encrypted_data}')
 
-    # save data to output, decoding the encrypted data into a str
-    decoded_secondary_encrypted = secondary_encrypted_data.decode()
-    f = open(output, 'w')
-    f.write(decoded_secondary_encrypted)
-    f.close()
-    print(f'[green]wrote decoded encrypted data to file [purple]{output}: [red]{decoded_secondary_encrypted}')
+        # save data to output, decoding the encrypted data into a str
+        decoded_secondary_encrypted = secondary_encrypted_data.decode()
+        f = open(output, 'w')
+        f.write(decoded_secondary_encrypted)
+        f.close()
+        print(f'[green]wrote decoded encrypted data to file [purple]{output}: [red]{decoded_secondary_encrypted}')
+
+    # if decrypt, decrypt and dump data
+    elif encrypt_or_decrypt == 'd':
+        secondary_decrypted = secondary_tool.decrypt(raw_data)
+        main_decrypted = main_tool.decrypt(secondary_decrypted)
+        main_decoded = main_decrypted.decode()
+        f = open(output, 'w')
+        f.write(main_decoded)
+        f.close()
+        print(f'[green]wrote decoded decrypted data to file [purple]{output}: [red]{main_decoded}')
+
+    elif encrypt_or_decrypt == 'r':
+        secondary_decrypted = secondary_tool.decrypt(raw_data)
+        main_decrypted = main_tool.decrypt(secondary_decrypted)
+        main_decoded = main_decrypted.decode()
+        f = open(output, 'w')
+        f.write(main_decoded)
+        f.close()
+        print(f'[green]wrote decoded decrypted data to file [purple]{output}: [red]{main_decoded}')
+        f = open(source, 'r')
+        raw_data = f.read()
+        f.close()
+        print(f'[green]read raw data from file [purple]{source}: [red]{raw_data}')
+        # encode, then encrypt data
+        encoded_data = raw_data.encode()
+        main_encrypted_data = main_tool.encrypt(encoded_data)
+        secondary_encrypted_data = secondary_tool.encrypt(main_encrypted_data)
+        print(f'[green]double encrypted data: [red]{secondary_encrypted_data}')
+
+        # save data to output, decoding the encrypted data into a str
+        decoded_secondary_encrypted = secondary_encrypted_data.decode()
+        f = open(output, 'w')
+        f.write(decoded_secondary_encrypted)
+        f.close()
+        print(f'[green]wrote decoded encrypted data to file [purple]{output}: [red]{decoded_secondary_encrypted}')
+
 encrypt_data()
