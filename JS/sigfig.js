fc0e7287bc8668169075a0ef8d376cde28ca30f0
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-09-10 22:03:45 +0000

    saving JS

--- JS/sigfig.js
+++ JS/sigfig.js
@@ -11,17 +11,6 @@ function display(text) {
 // main function
 function clickmeuwu() {
     // set up constants
-    const nonZeros = [
-        '1', 
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9'
-    ]
     const allowedChars = '.0123456789'
 
     //// set up flags
@@ -36,7 +25,7 @@ function clickmeuwu() {
     let leadingZeros = false
     let encounteredNonZeros = false
     let passedDecimal = false
-    let startingCount = false
+    let startedCount = false
     let enteredZeroZone = false
     // running count
     let saveCount = 0
@@ -55,14 +44,27 @@ function clickmeuwu() {
     // check if it contains a zero
     containsZero = inputSigFig.includes('0')
 
-    // if the message is only a period, return error
-    if (inputSigFig == ".") {
-        display('that is not a number!')
+    // if the message does not contain a valid character, error then return
+    splitInput.forEach(element => {
+        if (!(allowedChars.includes(element))) {
+            display('invalid characters included!')
+            return
+        }
+    })
+
+    // if the message is only decimals, error then return
+    onlyDecimalChar = true
+    splitInput.forEach(element => {
+        if (element != '.') {
+            onlyDecimalChar = false
+        }
+    })
+    if (onlyDecimalChar) {
+        display('there is only decimal points here!')
         return
     }
 
     // check if it contains a non zero
-    // nonZeros.forEach(element => {
     splitInput.forEach(element => {
         if (element != 0) {
             containsNonZero = true
@@ -82,103 +84,83 @@ function clickmeuwu() {
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// DETECTING THINGS
 
-        // if character is the decimal, passedDecimal = true. continue
-        if (character == '.') {
-            passedDecimal = true
-            continue
-        }
+        // if the current character is zero, currentIsZero = true
+        // currentIsNotZero is the opposite of currentIsZero
+        currentIsZero = character.includes('0')
+        currentIsNotZero = !currentIsZero
 
-        // if the current is zero, currentIsZero = true. currentIsNotZero = false
-        if (character == '0') {
-            currentIsZero = true
-            currentIsNotZero = false
-        }
+        // same above, if the current character is zero, then currentIsZero is true
+        // currentIsNotZero is the opposite of currentIsZero
+        currentIsZero = character.includes('0')
+        currentIsNotZero = !currentIsZero
 
-        // if the current is not zero, currentIsNotZero = true. currentIsZero = false
-        // if current is not zero, encounteredNonZeros = true
-        if (character != '0') {
-            currentIsNotZero = true
-            currentIsZero = false
-            encounteredNonZeros = true
-        }
+        // if current is not zero, then we have encounterd a zero
+        // so encounteredNonZeros is true
+        if (character != '0') {encounteredNonZeros = true}
 
-        // if encounteredNonZeros is false and character is zero, leadingZeros = true
+        // if you have not encountered a non zero, and the current character is 0, 
+        // then leading zeros is true, meaning that there are zeros at the front of the number
         if (!encounteredNonZeros && character == '0') {
             leadingZeros = true
         }
 
-        // if encounteredNonZeros is true and character is not zero, leadingZeros = false
+        // if we have encountered non zero characters 
+        // and the current character does not equal zero, 
+        // set leading zeros to false, meaning we have stopped the leading zeros
         if (encounteredNonZeros && character != '0') {
             leadingZeros = false
         }
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// USING LOGIC TO UPDATE COUNT
 
-        // if leadingZeros is true, dont count and instead just pass
+        // if the number has leading zeros, do not count 
+        // instead just increase the iterator to the next loop
         if (leadingZeros) {continue}
-        
-        // if we are out of any leading zeros
-        else {
 
-            // if the current number is not zero (double checking) and startingCount is false, increase currentCount and set startingCount to true
-            if (currentIsNotZero && !startingCount) {
-                startingCount = true
-            }
+        // all cases for if the current character is not a zero
+        if (currentIsNotZero) {
 
-            // if the current number is not a zero
-            if (currentIsNotZero) {
+            // if we have not started counting yet,
+            // set startedCount to true as we are going to count from this point on
+            if (!startedCount) {startedCount = true}
 
-                // if we have started counting, increase by 1
-                if (startingCount) {
-                    currentCount += 1
-                }
+            // if we have started counting and we have entered a zone
+            // where we have consistently hit zeros, then set 
+            // enteredZeroZone to false because we are out of zeros
+            // and have reached a non-zero character
+            else if (startedCount && enteredZeroZone) {enteredZeroZone = false}
+        }
 
-                // // if the current number is not a zero and starting count is true and entered zero zone is true, set entered zero zone to false and increase currentCount
-                else if (startingCount && enteredZeroZone) {
-                    enteredZeroZone = false
-                }
-            }
-
-            // if the current number is a zero
-            else if (currentIsZero) {
-                
-                // if starting count is true and enteredZeroZone is false, save currentCount to saveCount (in case we never find another nonzero) and set enteredZeroZone to true
-                // we set enteredZeroZone to true so this part does not repeat again
-                if (startingCount && !enteredZeroZone) {
-                    saveCount = currentCount
-                    enteredZeroZone = true
-                }
-
-                // if the current number is a zero and started counting is true, increase currentCount
-                if (startingCount) {
-                    currentCount += 1
-                }
+        // all cases for if the current character is a zero
+        else if (currentIsZero) {
+            
+            // if we have started counting and we have not entered a zero zone,
+            // set the saved count to the current count,
+            // then set entered zero zone to true
+            // we do this because we can potentially continue on with
+            // no following non-zero, and we don't count those zeros 
+            // but we only dont count the zeros if there is no decimal point
+            if (startedCount && !enteredZeroZone) {
+                saveCount = currentCount
+                enteredZeroZone = true
             }
         }
 
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// DEBUG LOGGING
-
-        // console.log('current is ' + character + ', and below are the states')
-        // // WORKS
-        // console.log('current is zero, ' + currentIsZero)
-        // console.log('current is not zero, ' + currentIsNotZero)
-        // console.log('contains zero, ' + containsZero)
-        // console.log('contains non zero, ' + containsNonZero)
-        // console.log('contains decimal, ' + containsDecimal)
-        // console.log('leading zeros, ' + leadingZeros)
-        // console.log('entered zero zone, ' + enteredZeroZone)
-        // console.log('current count, ' + currentCount)
-        // console.log('saved count, ' + saveCount)
-        // NOT YET
-    }
+        // if we have started counting, 
+        // increase the counter by 1
+        if (startedCount && !character.includes('.')) {currentCount += 1}
+    }    
     
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// POST LOOP LOGIC
 
-    // if the current one is zero, revert currentCount to the saved point (we never reached another nonzero number) and display, then return
-    if (currentIsZero || enteredZeroZone) {
-        display('the amount of significant figures is ' + saveCount)
+    // if the message does not contain a decimal
+    // and if we are in a zero zone
+    // display the save state as we ignore the zeros at the end
+    if (!containsDecimal && enteredZeroZone) {
+        display('the amount of significant figures is: ' + saveCount)
+        return
     }
     else {
-        display('the amount of significant figures is ' + currentCount)
+        display('the amount of significant figures is: ' + currentCount)
     }
 }
